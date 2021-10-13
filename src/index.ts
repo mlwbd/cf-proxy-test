842928d90439221ca9e98ae0398ca4dfaea4520c
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2021-10-13 20:19:36 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,9 @@ addEventListener('fetch', (event) => {
   const proxy = useProxy();
   proxy.use('/', {
     upstream: {
-      domain: 'httpbin.org',
+      domain: 'trac.ffmpeg.org/wiki',
       protocol: 'https',
     },
-
-    firewall: [
-      {
-        field: 'country',
-        operator: 'in',
-        value: ['CN', 'KP', 'SY', 'PK', 'CU'],
-      },
-    ],
   });
 
   const response = proxy.apply(event.request);
